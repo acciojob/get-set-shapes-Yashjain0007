96fb538d28127eaced899f5250b4046e1f28848e
Yashjain0007 <124593014+Yashjain0007@users.noreply.github.com>
2023-04-12 14:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width = width;
+		this.height = height;
+	}
+	get getters(){
+		return 
+	}
+	getArea(width,height){
+		let area = this.width * this.height;
+		return area;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(area){
+		this.area = area;
+	}
+	super(getArea());
+	getPerimrter(){
+		let perimeter = 4*this.area;
+		
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
